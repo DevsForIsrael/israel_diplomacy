955b3b787968716e044e22012bdb6ac866faea73
Nir Tzezana <nir@appcharge.com>
2023-10-08 16:15:11 +0300

    everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { Stack, Typography } from "@mui/material";
+import posts from "./posts.json";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//embed.redditmedia.com/widgets/platform.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Stack p={5}>
+        <Stack mb={1}>
+          <Typography variant='h1' fontSize={35} fontWeight={700}>
+            Help with Israeli Diplomacy across the Web
+          </Typography>
+        </Stack>
+        <Typography variant='h2' fontSize={25}>
+          Participate in debates and help spread the truth
+        </Typography>
+      </Stack>
+      <Stack p={5} gap={5} width='100%'>
+        {posts.map((post) => {
+          return (
+            <Stack>
+              <blockquote
+                className='reddit-embed-bq'
+                style={{ height: 500 }}
+                data-embed-height='240'
+              >
+                <a href={post}>{post}</a>
+              </blockquote>
+            </Stack>
+          );
+        })}
+      </Stack>
     </div>
   );
 }
